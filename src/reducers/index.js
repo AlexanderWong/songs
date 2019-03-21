31178478c2a694ffd1c1cb1e1ab729389e6de216
AlexanderWong <code.alexanderwong@gmail.com>
2019-03-21 11:54:34 -0700

    create reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,8 @@
-const songsReducer = () => {};
+const songsReducer = () => {
+  return [
+    { title: "Black Hole Sun", duration: "5:19" },
+    { title: "Black", duration: "5:43" },
+    { title: "Rooster", duration: "6:14" },
+    { title: "Lithium", duration: "4:17" }
+  ];
+};
